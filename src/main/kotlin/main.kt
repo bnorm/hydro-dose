b412d773de2a2a7e721ff60b85bc793eba9d1fa5
Brian Norman <brian.norman@datasite.com>
2021-03-17 22:04:36 -0500

    Periodically print Pi4J context

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,21 @@
+import com.pi4j.Pi4J
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 
 @OptIn(ExperimentalTime::class)
 fun main() {
-    val mark = TimeSource.Monotonic.markNow()
-    while (true) {
-        println("Elapsed time: ${mark.elapsedNow()}")
-        Thread.sleep(5_000)
+    val pi4j = Pi4J.newAutoContext()
+    try {
+        val mark = TimeSource.Monotonic.markNow()
+        while (true) {
+            pi4j.describe().print(System.out)
+
+            repeat(24) {
+                println("Elapsed time: ${mark.elapsedNow()}")
+                Thread.sleep(5_000)
+            }
+        }
+    } finally {
+        pi4j.shutdown()
     }
 }
